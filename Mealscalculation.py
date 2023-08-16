0da1cf4d33f79fda6e97b7e0016d2d742c059497
Sangeetha <140884105+SangeethaMP94@users.noreply.github.com>
2023-08-16 19:34:49 +0530

    Create Mealscalculation.py

---
+++ Mealscalculation.py
@@ -0,0 +1,7 @@
+meals = int(input("No of meals: "))
+meals_price = 70
+juice = int(input("No of Juices: "))
+juice_price = 50
+snacks = int(input("No of Snacks: "))
+snacks_price = 30
+print("Total Amount: ",(meals*meals_price)+(juice*juice_price)+(snacks*snacks_price))
